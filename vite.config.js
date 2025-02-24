5f5bd9e011d5ba40eeba4ca5f79ff1550c18db0d
jones-31 <jonesvimal311@gmail.com>
2025-02-24 13:15:04 +0530

    staged all

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"), 
+  },
   build: {
     lib: {
       entry: "./src/button.jsx",
