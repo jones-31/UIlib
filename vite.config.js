4920ddda8f72de22b6176450a397ade0a89523fc
jones-31 <jonesvimal311@gmail.com>
2025-02-24 14:40:13 +0530

    stagd all

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
+import replace from '@rollup/plugin-replace';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    "process.env": {}, 
-    "process.env.NODE_ENV": JSON.stringify("production"),
-  },
+  plugins: [react(),
+    replace({
+      preventAssignment: true,
+      values: {
+        'process.env.NODE_ENV': JSON.stringify('production'), // ✅ Fix process issue
+      },
+    }),
+  ],
+  // define: {
+  //   "process.env": {}, 
+  //   "process.env.NODE_ENV": JSON.stringify("production"),
+  // },
   build: {
     lib: {
       entry: "./src/button.jsx",
